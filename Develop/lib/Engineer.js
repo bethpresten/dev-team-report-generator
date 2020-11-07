e4a5d3dd4b22d6b7453c0c4011853a6dbb00350b
Beth Presten <bethpresten@gmail.com>
2020-11-07 16:49:18 -0500

    employee.js now passes all test, engineer passes almost all tests except for getGithub

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+// const Engineer = new Employee();
+
+class Engineer extends Employee {
+    constructor(github) {
+        super(name, id, email);
+        this.role = "Engineer";
+        this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    };
+
+    getRole() {
+        return this.role;
+    };
+
+}
+
+const engineer = new Employee();
+// const engineer = new Engineer();
+engineer.getId();
+engineer.getGithub();
+
+module.exports = Engineer;

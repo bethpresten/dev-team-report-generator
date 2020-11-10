0b733b1c3abc7056363252854b91144551ea3a9f
Beth Presten <bethpresten@gmail.com>
2020-11-10 10:04:44 -0500

    added comments explaining the app.js code

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+// code that was given to us; calling each one of the constructors, utilizing fs and path, naming the folder and the direct file
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,7 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+// set of questions that any user will use/ inquirer.prompt utilizes node to prompt the user via command line
 const employeeQuestions = () =>
   inquirer.prompt
     ([
@@ -36,6 +37,7 @@ const employeeQuestions = () =>
         message: "What is your email address?",
       }
     ])
+    // identifying the role and then sending back user-specific question(s)
     .then((answers) => {
       if (answers.role === "manager") {
         return renderManager(answers)
@@ -45,15 +47,17 @@ const employeeQuestions = () =>
         return renderEngineer()
       }
       return (employeeQuestions.role);
+      // pulling the answers, console logging the answers, and then writing the file
     }).then(function (answers) {
       console.log(answers);
       writeHTML(outputPath, answers)
     });
 
-
+//calling the function to deliver the questions to the user
 employeeQuestions();
+// empty array to dump all the objects
 const employees = [];
-
+// manager-specific questions plus adding in additional team members
 const renderManager = (answers) => {
   return inquirer.prompt([
     {
@@ -67,10 +71,12 @@ const renderManager = (answers) => {
       message: "Would you like to add another employee??",
       choices: ["yes", "no"]
     },
+    // pushing all the information to the constructor
   ]).then(function (managerAnswers) {
     const manager = new Manager(answers.name, answers.role, answers.id, answers.email, managerAnswers.officeNumber);
     employees.push(manager);
 
+    // asking to add additional employees or writing to the HTML if finished
     console.log(answers);
     if (answers.addEmployee === "yes") {
       return employeeQuestions();
@@ -80,6 +86,7 @@ const renderManager = (answers) => {
   });
 };
 
+// intern specific questions delivered via node
 const renderIntern = () => {
   return inquirer.prompt([
     {
@@ -93,10 +100,11 @@ const renderIntern = () => {
       message: "Would you like to add another employee??",
       choices: ["yes", "no"]
     },
+    // writing all the answers to the constructor and pushing it to the empty array
   ]).then(function (internAnswers) {
     const intern = new Intern(answers.name, answers.role, answers.id, answers.email, internAnswers.officeNumber);
     employees.push(intern);
-
+    // identifying if the user wants to add more team members
     console.log(answers);
     if (answers.addEmployee === "yes") {
       return employeeQuestions();
@@ -105,7 +113,7 @@ const renderIntern = () => {
     }
   });
 };
-
+// engineer specific questions delivered via node
 const renderEngineer = () => {
   return inquirer.prompt([
     {
@@ -119,10 +127,11 @@ const renderEngineer = () => {
       message: "Would you like to add another employee??",
       choices: ["yes", "no"]
     },
+    // pushing engineer questions to the empty array
   ]).then(function (engineerAnswers) {
     const engineer = new Engineer(answers.name, answers.role, answers.id, answers.email, engineerAnswers.officeNumber);
     employees.push(manager);
-
+    // asking if the user needs to add additional team members
     console.log(answers);
     if (answers.addEmployee === "yes") {
       return employeeQuestions();
@@ -133,9 +142,8 @@ const renderEngineer = () => {
 };
 
 
-
+// function to write to the team.html
 function writeHTML() {
-
   fs.writeFile(outputPath, render(employees), null, function (err) {
     if (err) {
       return console.log(err);
